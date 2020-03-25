6d9c9b8c3bdda697c1e350f2ca7e6649d113b304
MartinHeinz <martin7.heinz@gmail.com>
2020-03-25 20:45:12 +0100

    Switch to Flask app for k8s demonstration.

--- blueprint/__main__.py
+++ blueprint/__main__.py
@@ -1,4 +1,12 @@
-from .app import Blueprint
+from flask import Flask, request
 
-if __name__ == '__main__':
-    Blueprint.run()
+app = Flask(__name__)
+
+
+@app.route("/")
+def echo():
+    return f"You said: {request.args.get('text', '')}\n"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
